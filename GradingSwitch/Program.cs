2e79656a24b8a2bde50e8eb45d5e255c304ce3b1
Kasutaja Kasutaja <ajurgens@tktk.ee>
2022-03-18 18:11:37 +0200

    add new projects

---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai;
+            //kui kasutaja sai "A", konsool kuvab "Suurepärane";
+            //"B", konsool kuvab "Väga hea!";
+            //"C", konsool kuvab "Hea!";
+            //"D", konsool kuvab "Rahulav!";
+            //"E", konsool kuvab "Kasin!";
+            //"F", konsool kuvab "Puudulik";
+            //kui kasutaja sisestab mmidagi muud, siis konsool kuvab "Vale väärtus"
+
+            //kasutame Switch
+
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            //char - character
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+
+            switch (userColor)
+
+            {
+            case 'A':
+                Console.WriteLine("Suurepärane!");
+                    break;
+
+            case 'B':
+                    Console.WriteLine("Väga hea!");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("Hea!");
+                    break;
+
+                case 'D':
+                Console.WriteLine("Rahuldav!");
+                    break;
+
+                case 'E':
+                    Console.WriteLine("Kasin!");
+                    break;
+
+                case 'F':
+                    Console.WriteLine("Puudulik!");
+                    break;
+                default:
+                    Console.WriteLine("Vale väärtus!");
+                    break;
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
